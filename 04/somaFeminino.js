5de8ad370f05fe18cec954dfb8805ad3b6f0a50c
Igor Matos <sarkan.igor@gmail.com>
2023-01-01 23:24:46 -0300

    atividadePratica11ArraysObjetos

---
+++ 04/somaFeminino.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+    nome: 'Roger Medeiros',
+    sexo: 'M',
+    salario: 3250,
+    },
+    {
+    nome: 'Carolina Silva',
+    sexo: 'F',
+    salario: 1200,
+    },
+    {
+    nome: 'Cristina Avila',
+    sexo: 'F',
+    salario: 8100,
+    },
+    {
+    nome: 'Gustavo Hoffman',
+    sexo: 'M',
+    salario: 5200.35,
+    },
+    {
+    nome: 'Eva Trindade',
+    sexo: 'F',
+    salario: 2501,
+    },
+    {
+    nome: 'Andre Mathias',
+    sexo: 'M',
+    salario: 1750,
+    },
+    {
+    nome: 'Joice Castro da Silva',
+    sexo: 'F',
+    salario: 3350.25,
+    },
+    ];
+
+// 4. Imprima no console a soma do salario de todos as pessoas do sexo Masculino
+
+const sexoMasculino = data.filter ((valor, indice, array) => valor.sexo === 'M' )
+const somaSalarioMasculino = sexoMasculino.reduce ((soma, valor) => soma += valor.salario ,0)
+
+console.log(`A soma dos salario das mulheres são ${somaSalarioMasculino}`)
